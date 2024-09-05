94262195afa4f7d6562ebcd951fb2efe10dd2d2e
ANTHONY ABAH <81562801+TONYABAH@users.noreply.github.com>
2024-09-05 11:18:38 +0100

    Removed capacitor support
    Removed capacitor support

--- src/components/charts/BarChart.vue
+++ src/components/charts/BarChart.vue
@@ -14,7 +14,8 @@ import {
   CategoryScale,
   LinearScale,
 } from "chart.js";
-
+const id = "_" + Math.random().toString(16).substring(3);
+var mixedChart = null
 ChartJS.register(
   Title,
   Tooltip,
@@ -23,30 +24,54 @@ ChartJS.register(
   CategoryScale,
   LinearScale
 );
-const id = "_" + Math.random().toString(16).substring(3);
+
 function drawChart() {
+
   const ctx = document.getElementById(id);
   const data = {
     labels: ["January", "February", "March", "April"],
+
     datasets: [
-      {
-        type: "bar",
-        label: "Complaints",
-        data: [10, 20, 30, 40],
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-      },
       {
         type: "line",
         label: "Raids",
         data: [10, 20, 40, 30],
         fill: false,
-        borderColor: "rgb(54, 162, 235)",
+       // borderColor: "rgb(54, 162, 235)",
+        borderColor: 'rgb(54, 162, 25)',
+        backgroundColor:'rgb(54, 162, 25)',
         tension: 0.3,
+        hoverOffset: 4
       },
+      {
+        type: "bar",
+        label: "Complaints",
+        data: [10, 20, 30, 40],
+        borderColor: "rgb(255, 99, 132)",
+        //backgroundColor: "rgba(255, 99, 132, 0.2)",
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 99, 132)',
+          'rgb(255, 205, 86)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      },
+
     ],
   };
-  const mixedChart = new ChartJS(ctx, {
+  const plugin = {
+    id: 'customCanvasBackgroundColor',
+    beforeDraw: (chart, args, options) => {
+      const {ctx} = chart;
+      ctx.save();
+      ctx.globalCompositeOperation = 'destination-over';
+      ctx.fillStyle = options.color || '#99ffff';
+      ctx.fillRect(0, 0, chart.width, chart.height);
+      ctx.restore();
+    }
+  };
+  mixedChart = new ChartJS(ctx, {
     type: "bar",
     data,
     options: {
@@ -57,46 +82,24 @@ function drawChart() {
           beginAtZero: true,
         },
       },
+      plugins: {
+        customCanvasBackgroundColor: {
+          color: '',
+        }
+      }
     },
+    //plugins: [plugin],
   });
 }
 
-//const options = chartConfig.options
-/*options = {
-  responsive: true,
-  maintainAspectRatio: false
-}*/
-/**
- * randomdata = {
-  labels: [
-    'January' + getRandomInt(),
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December'
-  ],
-  datasets: [
-    {
-      label: 'Data One',
-      backgroundColor: '#f87979',
-      data: [
-        getRandomInt(),
-      ]
-    }
-  ]
-})
- */
 onMounted(() => {
-  setInterval(() => {
+  setTimeout(() => {
+    if(mixedChart)mixedChart.destroy()
     drawChart();
-  }, 3000);
+    mixedChart.resize(0, 300);
+  }, 1);
 });
-onBeforeMount(() => {});
+onBeforeMount(() => {
+  if(mixedChart)mixedChart.destroy()
+});
 </script>
--- src/components/dashboard/MainDashboard.vue
+++ src/components/dashboard/MainDashboard.vue
@@ -1,18 +1,7 @@
 <template>
   <q-layout view="hHr LpR lFf">
-    <!--<q-header v-model="header" reveal elevated bordered>
-
-    </q-header>-->
     <DefaultHeader />
-
-    <q-footer v-model="footer" reveal elevated bordered>
-      <q-toolbar class="bg-purple text-white">
-        <q-btn flat round dense icon="assignment_ind" />
-        <q-toolbar-title> Footer </q-toolbar-title>
-        <q-btn flat round dense icon="apps" class="q-mr-xs" />
-        <q-btn flat round dense icon="more_vert" />
-      </q-toolbar>
-    </q-footer>
+    <DefaultFooter />
     <q-drawer
       side="left"
       v-model="drawerLeft"
@@ -34,29 +23,29 @@
             :class="$q.screen.lt.sm ? 'q-col-gutter-xs' : 'q-col-gutter-md'"
           >
             <div class="col col-xs-12 col-sm-12 col-md-6 col-lg-4">
-              <div id="google_chart_div">
-                <q-card class="my-card">
-                  <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
-                  <q-card-section>
-                    <div class="text-h6">Mails</div>
-                  </q-card-section>
-                  <q-card-section>
-                    <BarChart :options="{}" />
-                  </q-card-section>
-                </q-card>
-              </div>
+              <q-card class="my-card full-height">
+                <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
+                <q-card-section>
+                  <div class="text-h6">Mails</div>
+                </q-card-section>
+                <q-card-section>
+                  <BarChart :options="{}" />
+                </q-card-section>
+              </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-6 col-lg-4">
-              <q-card class="my-card">
+              <q-card class="my-card full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Raids</div>
                 </q-card-section>
-                <q-card-section> </q-card-section>
+                <q-card-section>
+                  <Barchart3D />
+                </q-card-section>
               </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-4">
-              <q-card class="my-card">
+              <q-card class="my-card full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Surveillances</div>
@@ -65,7 +54,7 @@
               </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-4">
-              <q-card class="my-card">
+              <q-card class="my-card full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Samples</div>
@@ -74,7 +63,7 @@
               </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-3">
-              <q-card class="my-card">
+              <q-card class="my-card full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Accounts</div>
@@ -83,7 +72,7 @@
               </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-4">
-              <q-card class="my-card full-height">
+              <q-card class="my-card full-height full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Complaints</div>
@@ -92,7 +81,7 @@
               </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-8 col-lg-4">
-              <q-card class="my-card">
+              <q-card class="my-card full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Destructions</div>
@@ -101,7 +90,7 @@
               </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-8 col-lg-4">
-              <q-card class="my-card full-height">
+              <q-card class="my-card full-height full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Detentions</div>
@@ -110,7 +99,7 @@
               </q-card>
             </div>
             <div class="col col-xs-12 col-sm-6 col-md-4 col-lg-4">
-              <q-card class="my-card">
+              <q-card class="my-card full-height">
                 <!--<img src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
                 <q-card-section>
                   <div class="text-h6">Cases</div>
@@ -140,7 +129,9 @@
 import { onMounted, computed, ref } from "vue";
 import * as dashboard from "./dashboard";
 import DefaultHeader from "src/layouts/DefaultHeader.vue";
+import DefaultFooter from "src/layouts/DefaultFooter.vue";
 import BarChart from "../charts/BarChart.vue";
+import Barchart3D from "../charts/Barchart3D.vue";
 // Load the Visualization API and the corechart package.
 //import ScatterChartViewer from "./ScatterChartViewer.vue";
 //import PieChart3DhartViewer from "./PieChart3DViewer.vue";
@@ -149,54 +140,7 @@ import BarChart from "../charts/BarChart.vue";
 //import AnalogClock from "../AnalogClock.vue";
 //import { useDefaultStore } from "src/stores/store";
 //const store = useDefaultStore();
-const header = ref(true);
-const footer = ref(true);
 const drawerLeft = ref(false);
-
-const geoChartOptions = {
-  chart: {
-    title: "Company Performance",
-    subtitle: "Sales, Expenses, and Profit: 2014-2017",
-  },
-  region: "NG",
-  displayMode: "text",
-  //colorAxis: { colors: ["#00853f", "black", "#e31b23"] },
-  backgroundColor: "#81d4fa",
-  datalessRegionColor: "#efefef",
-  defaultColor: "#f5f5f5",
-  showTooltip: true,
-  showInfoWindow: true,
-  useMapTypeControl: true,
-  icons: {
-    default: {
-      normal:
-        "https://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Push-Pin-1-Chartreuse-icon.png",
-      selected:
-        "https://icons.iconarchive.com/icons/icons-land/vista-map-markers/48/Map-Marker-Push-Pin-1-Right-Chartreuse-icon.png",
-    },
-  },
-  maps: {
-    styledMap: {
-      name: "Styled",
-      styles: [
-        { featureType: "poi.attraction", stylers: [{ color: "#fce8b2" }] },
-        {
-          featureType: "road.highway",
-          stylers: [{ hue: "#0277bd" }, { saturation: -50 }],
-        },
-        {
-          featureType: "road.highway",
-          elementType: "labels.icon",
-          stylers: [{ hue: "#000" }, { saturation: 100 }, { lightness: 50 }],
-        },
-        {
-          featureType: "landscape",
-          stylers: [{ hue: "#259b24" }, { saturation: 10 }, { lightness: -22 }],
-        },
-      ],
-    },
-  },
-};
 
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
@@ -226,16 +170,16 @@ function drawChart() {
   });
 }
 onMounted(() => {
-  const year = computed(() => new Date().getFullYear());
-  google.charts.load("current", { packages: ["corechart"] });
+  //const year = computed(() => new Date().getFullYear());
+  //google.charts.load("current", { packages: ["corechart"] });
   // Set a callback to run when the Google Visualization API is loaded.
-  google.charts.setOnLoadCallback(drawChart);
+  //google.charts.setOnLoadCallback(drawChart);
 });
 </script>
 
 <style>
 .dashboard .q-card {
-  min-height: 50vh;
+  min-height: 70vh;
 }
 .dashboard .col {
   /*box-shadow: 1px 1px 1px 1px #777;*/
